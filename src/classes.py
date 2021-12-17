ee4ed45e9d7d2ee0e485a5d070940ecd6de68a97
youcefAZ <youcefdota@gmail.com>
2021-12-17 16:37:07 +0100

    bug fix for classes

--- src/classes.py
+++ src/classes.py
@@ -6,11 +6,11 @@ class hopital:
         self.list_urgence=list_urgence
         self.salles_urgence=salles_urgence
     
-    def add_service(service):
+    def add_service(self,service):
         #add new services
         pass
     
-    def remove_service(service_name):
+    def remove_service(self,service_name):
         #remove service
         pass
 
@@ -32,15 +32,15 @@ class pharmacie:
         self.position=position
         self.list_medicaments=list_medicaments
     
-    def check_availability(ordonnance):
+    def check_availability(self,ordonnance):
         # we check if the medicaments in ordonnance are available in list_medicaments
         pass
 
-    def add_medicament(medicament):
+    def add_medicament(self,medicament):
         #we add medicament in list_medicaments, if it already exists we add in quantity
         pass
 
-    def remove_medicament(medicament_name):
+    def remove_medicament(self,medicament_name):
         #we delete a medicament when the stock runs out, or we decrement only if it doesnt run out
         pass
 
