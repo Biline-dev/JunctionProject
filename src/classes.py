3bc750594f57286969c44655403c54ff5965ff69
Benzeghiba Mohamed Tayeb <32774937+TayebBenz@users.noreply.github.com>
2021-12-18 06:49:52 +0100

    Add files via upload

--- src/classes.py
+++ src/classes.py
@@ -1,8 +1,8 @@
 
 class hopital:
-    def __init__(self, name, emergency_id="11",patient_id=1):
+    def __init__(self, name, list_services=[] ,emergency_id="11",patient_id=1):
         self.name=name
-        self.list_services=[]
+        self.list_services= list_services
         self.list_urgence=[]
         self.salles_urgence=[]
         self.emergency_id = emergency_id
@@ -11,6 +11,9 @@ class hopital:
     def add_service(self,service):
         self.list_services.append(service)
         pass
+
+    def set_emergency_list(self,list_urgence):
+        self.list_urgence = list_urgence
 
     def remove_service(self,service_name):
         service = search_service(self,service_name)
@@ -66,87 +69,23 @@ class pharmacie:
         self.name=name
         self.position=position
         self.list_medicaments=list_medicaments
-        
-    def __repr__(self):
-        return (self.name)
-    
+
     def check_availability(self,ordonnance):
         # we check if the medicaments in ordonnance are available in list_medicaments
-        cmp=0
-        for med in ordonnance:
-            for lst in self.list_medicaments:
-                if med.if_same_med(lst):
-                    cmp+=1
-                
-
-        return cmp
-
-    def return_intersection(self,ordonnance):
-        #we return intersection between dispo medics and medics in ordonnance
-        intersect=[]
-        for med in ordonnance:
-            if med.if_med_inlist(self.list_medicaments):
-                intersect.append(med)
-
-        return intersect
-    
-    def return_intersection_bar(self,ordonnance):
-        #we return intersection between dispo medics and medics in ordonnance
-        intersect=[]
-        for med in ordonnance:
-            if not(med.if_med_inlist(self.list_medicaments)):
-                intersect.append(med)
-
-        return intersect
-
+        pass
 
     def add_medicament(self,medicament):
         #we add medicament in list_medicaments, if it already exists we add in quantity
-        bool=False
-        for medic in self.list_medicaments:
-            if medic.if_same_med(medicament):
-                medic.quantity+=1
-                bool=True
-                break
-        
-        if bool==False:
-            self.list_medicaments.append(medicament)
+        pass
 
-
-
-    def remove_medicament(self,medicament):
+    def remove_medicament(self,medicament_name):
         #we delete a medicament when the stock runs out, or we decrement only if it doesnt run out
-        for medic in self.list_medicaments:
-            if medic.if_same_med(medicament):
-                if medic.quantity==1 or medic.quantity==0:
-                    self.list_medicaments.remove(medic)
-                
-                else:
-                    medic.quantity-=1
-            
+        pass
 
 
 
 class medicament:
-    def __init__(self,name,dose=None,quantity=None):
+    def __init__(self,name,dose,quantity):
         self.name=name
         self.dose=dose
         self.quantity=quantity
-
-    def __repr__(self):
-        return 'name=' + str(self.name) + ', dose='+ str(self.dose)
-    
-    def if_same_med(self, medicament):
-        if self.name==medicament.name and self.dose==medicament.dose:
-            return True
-        
-        return False
-
-
-    def if_med_inlist(self, list_med):
-
-        for medec in list_med:
-            if self.if_same_med(medec):
-                return True
-
-        return False
--- src/tests.py
+++ src/tests.py
@@ -3,33 +3,41 @@ import classes as classes
 def reserve(service_name,hospital):
     for service in hospital.list_services:
         if(service.name == service_name):
-            new_patient(service.check_in())
+            patient_id , len = service.check_in()
+            # print("your id is %d and there is %d people before you in line" %(patient_id,len))
 
-def new_patient(information):
-    patient_id , len = information
-    print("your id is %d and there is %d people before you in line" %(patient_id,len))
 
-def new_emergency_patient(information):
-    patient_id , len = information
-    if len > reasonable_waiting_time:
-        # nearby_hospitals_available()
-        pass
-    else:
-        print("your id is %d and there is %d people before you in  line" %(patient_id,len))
+def new_emergency_patient(hospital):
+    patient_id , lenght = hospital.new_emergency_patient()
+    print("your id is %d and there is %d people before you in line" %(patient_id,lenght))
+    if lenght > reasonable_waiting_time:
+        proposed_hospital = hospital
+        for nearby_hospital in  hospitals_list:
+            if nearby_hospital != hospital and lenght > len(nearby_hospital.list_urgence):
+                proposed_hospital = nearby_hospital
+        if(proposed_hospital != hospital):
+            print("we suggest you go to %s where the waiting line for emergency is %s" %(proposed_hospital.name,len(proposed_hospital.list_urgence)))
 
 def patient_called(service_name,hospital):
     for service in hospital.list_services:
         if(service.name == service_name):
             patient_id = service.patient_called()
-    print("patient %d please procced to the doctor" %(patient_id))
+    # print("patient %d please procced to the %s" %(patient_id,service_name))
 
-reasonable_waiting_time = 5
+reasonable_waiting_time = 2
 
 services = []
-h1 = classes.hopital("hopital 1")
+h1 = classes.hopital("Hospital Aïn Naadja")
+h2 = classes.hopital("Hospital Mohamad Lamine")
+h3 = classes.hopital("Hospital Salim Zmirli")
 h1.add_service(classes.service("dentist"))
 h1.add_service(classes.service("ophtalmologue"))
+h2.add_service(classes.service("ophtalmologue"))
+h3.add_service(classes.service("ophtalmologue"))
+h2.set_emergency_list([111,112,113,114,115,116])
+h3.set_emergency_list([111])
 
+hospitals_list = [h1 , h2, h3]
 
 reserve("dentist",h1)
 reserve("ophtalmologue",h1)
@@ -40,18 +48,7 @@ patient_called("ophtalmologue",h1)
 reserve("dentist",h1)
 reserve("dentist",h1)
 reserve("ophtalmologue",h1)
-#
-# new_emergency_patient(h1.new_emergency_patient())
-# reserve("ophtalmologue",h1)
-# reserve("ophtalmologue",h1)
-# patient_called("ophtalmologue",h1)
-# patient_called("ophtalmologue",h1)
-# reserve("dentist",h1)
-# reserve("dentist",h1)
-# reserve("ophtalmologue",h1)
-# patient_called("ophtalmologue",h1)
-# reserve("dentist",h1)
-# reserve("ophtalmologue",h1)
-# reserve("dentist",h1)
-# reserve("ophtalmologue",h1)
-# patient_called("ophtalmologue",h1)
+new_emergency_patient(h1)
+new_emergency_patient(h1)
+new_emergency_patient(h1)
+new_emergency_patient(h1)
